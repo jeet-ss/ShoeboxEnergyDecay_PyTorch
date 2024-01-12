04f225f6cfa38e30ba2096d3e1b7cf17a8b257ba
jeet-ss <jeet.sensarma@gmail.com>
2024-01-12 11:37:22 +0100

    job scripts added

---
+++ cpu_job.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -l 
+#SBATCH --cpus-per-task=8
+#SBATCH --time=05:00:00 
+#SBATCH --job-name=gen_data
+#SBATCH --export=NONE 
+
+unset SLURM_EXPORT_ENV 
+
+# cpus-per-task has to be set again for srun
+export SRUN_CPUS_PER_TASK=$SLURM_CPUS_PER_TASK
+
+#module load python/3.10-anaconda
+source $HOME/.rir/bin/activate
+cd $HPCVAULT/ShoeboxEnergyDecay_PyTorch
+#
+srun python generate_data.py
---
+++ gpu_job.sh
@@ -0,0 +1,18 @@
+#!/bin/bash -l 
+#SBATCH --cpus-per-task=8
+#SBATCH --gres=gpu:1
+#SBATCH --time=10:59:00 
+#SBATCH --job-name=rir
+#SBATCH --export=NONE 
+
+unset SLURM_EXPORT_ENV 
+
+# cpus-per-task has to be set again for srun
+export SRUN_CPUS_PER_TASK=$SLURM_CPUS_PER_TASK
+
+#module load python/3.10-anaconda
+source $HOME/.rir/bin/activate
+cd $HPCVAULT/ShoeboxEnergyDecay_PyTorch
+#ls
+#python --version
+srun python train.py
