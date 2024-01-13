951da7afb1c106f76ebdc2570ff7d086a9302b28
jeet-ss <jeet.sensarma@gmail.com>
2024-01-13 14:09:57 +0100

    test noise level

--- generate_StochasticRIR_del.py
+++ generate_StochasticRIR_del.py
@@ -3,11 +3,13 @@ import torch
 from util.applyPressureSource import apply_pressure_source
 from util.analyticDampingDensity import analytic_damping_density
 
-def generate_stochastic_rir_del(del_Kx, del_Ky, del_Kz, fs=48000, c=343, use_pressure_source=False, device='cpu'):
+def generate_stochastic_rir_del(del_Kx, del_Ky, del_Kz, noise_level, fs=48000, c=343, use_pressure_source=False, device='cpu'):
     ###
     #
     # This function uses the difference of K's to predict in the correct order
     ####
+    # 
+    #noise_param = 0.0001
     # Predefine L 
     L = torch.tensor([3,4,5])
     V = torch.prod(L)
@@ -34,14 +36,15 @@ def generate_stochastic_rir_del(del_Kx, del_Ky, del_Kz, fs=48000, c=343, use_pre
     # if H.requires_grad : H.register_hook(lambda x : print("H", H.grad_fn,torch.any(torch.isnan(x))))
     # uniform sampling of damping density for the decay envelope
     time = torch.arange(1, max_time * fs + 1).to(device=device) / fs
-    envelope = torch.sqrt(torch.clamp(torch.exp(c * time.unsqueeze(1) * sigma) @ H * torch.mean(torch.diff(sigma)), min=torch.tensor(eps).to(device=device)) )
+    envv = torch.sqrt(torch.clamp(torch.exp(c * time.unsqueeze(1) * sigma) @ H * torch.mean(torch.diff(sigma)), min=torch.tensor(eps).to(device=device)) )
+    envelope = envv #+ torch.exp(noise_level if torch.is_tensor(noise_level) else torch.tensor(noise_level)) # torch.exp()
     # envelope = torch.sqrt(torch.exp(c * time.unsqueeze(1) * sigma) @ H * torch.mean(torch.diff(sigma)))
-
+    # if envelope.requires_grad : envelope.register_hook(lambda x : print("envelope", envelope.grad_fn,torch.any(torch.isnan(x))))
     # shape noise
-    h = envelope * torch.randn(len(time)).to(device=device)
+    #h = envelope * torch.randn(len(time)).to(device=device)
     # if h.requires_grad : h.register_hook(lambda x : print("h", h.grad_fn, torch.any(torch.isnan(x))))
     # apply pressure source to match the color, but compensate the energy loss
     if use_pressure_source:
         h = apply_pressure_source(h)
 
-    return h
+    return envelope
